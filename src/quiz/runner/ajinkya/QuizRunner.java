192b68b22a861c6e538b7385c99eee9081c129fd
Ajinkya Vaze <ajinkyav@gmail.com>
2017-05-03 17:46:00 +0530

    Quiz 2 answers

--- src/quiz/runner/ajinkya/QuizRunner.java
+++ src/quiz/runner/ajinkya/QuizRunner.java
@@ -1,10 +1,13 @@
 package quiz.runner.ajinkya;
 
-import java.util.Collections;
-import java.util.List;
-
+import quiz.model.Ground;
+import quiz.model.Match;
 import quiz.model.QuizDataProvider;
 import quiz.model.Team;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 public class QuizRunner {
 
 	private QuizRunner() {
@@ -14,6 +17,11 @@ public class QuizRunner {
 	public static void main(String[] args) {
 
 		List<Team> teams = QuizDataProvider.getTeams();
+		//quiz1(teams);
+		quiz2(teams);
+	}
+
+	private static void quiz1(List<Team> teams) {
 		// 1
 		teams.stream().map(team -> {
 			return new StringBuilder(team.getName()).append(": ").append(team.getHomeGround()).toString();
@@ -33,6 +41,39 @@ public class QuizRunner {
 		teams.stream().map(team -> {
 			return team.getHomeGround().name();
 		}).sorted().forEach(System.out::println);
-		
+	}
+	
+	private static void quiz2(List<Team> teams) {
+		// 1
+		teams.stream().collect(Collectors.toMap(Team::getName, Team::getHomeGround)).entrySet().forEach(System.out::println);
+
+		// 2
+		teams.stream()
+				.filter(team -> QuizDataProvider.TEAM1.equals(team.getName()))
+				.flatMap(team -> team.getMatches().stream())
+				.collect(Collectors.groupingBy(Match::getVenue, Collectors.counting()))
+				.entrySet().stream()
+				.forEach(System.out::println);
+
+		// 3
+		System.out.println(teams.stream()
+				.flatMap(team -> team.getMatches().stream())
+				.filter(match -> QuizDataProvider.TEAM1.equals(match.getOpponent()))
+				.mapToInt(Match::getRunsScored)
+				.sum());
+
+		// 4
+		boolean playedAtVenue = teams.stream()
+				.filter(team -> QuizDataProvider.TEAM1.equals(team.getName()))
+				.flatMap(team -> team.getMatches().stream())
+				.anyMatch(match -> Ground.WANKHEDE == match.getVenue());
+		System.out.println(playedAtVenue ? "YES": "NO");
+
+		// 5
+		System.out.println(teams.stream()
+				.filter(team -> QuizDataProvider.TEAM1.equals(team.getName()))
+				.flatMap(team -> team.getMatches().stream())
+				.filter(match -> match.getRunsScored() > match.getRunsConceded())
+				.count());
 	}
 }
