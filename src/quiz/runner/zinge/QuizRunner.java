270da6898b3d31598db2c25d33e6b1f8b22c965c
Ghansham Zinge <gzinge@pdxinc.com>
2017-05-03 23:52:31 +0530

    quiz 1

--- src/quiz/runner/zinge/QuizRunner.java
+++ src/quiz/runner/zinge/QuizRunner.java
@@ -1,5 +1,6 @@
 package quiz.runner.zinge;
 
+import java.util.Comparator;
 import java.util.List;
 
 import quiz.model.QuizDataProvider;
@@ -13,7 +14,40 @@ public class QuizRunner {
 	public static void main(String[] args) {
 
 		List<Team> teams = QuizDataProvider.getTeams();
-		System.out.println(teams);
+		//System.out.println(teams);
+		quiz_one(teams);
 	}
+	
+	private static void quiz_one(List<Team> teams){
+		
+		//1.Print all the team name and home ground. Sample output – "Rising Super Giants : GAHUNJE".
+		System.out.println("1)=============  Print all the team name and home ground =========================");
+		teams.stream()
+		.map(x -> x.getName() + " : "+x.getHomeGround())
+		.forEach(System.out::println);
+		
+		//2.	Print the team names in natural sorted order.
+		System.out.println("\n\n2)=============  Print the team names in natural sorted order =================");
+		teams.stream()
+		.sorted(Comparator.comparing(Team::getName))
+		.map(x->x.getName())
+		.forEach(System.out::println);
+		
+		//3.	Print the team names in reverse sorted order
+		System.out.println("\n\n3)=============  Print the team names in reverse sorted order =================");
+		teams.stream()
+		.sorted(Comparator.comparing(Team::getName).reversed())
+		.map(x->x.getName())
+		.forEach(System.out::println);
+		
+		//4.	Print the home ground names in alphabetical order.
+		System.out.println("\n\n3)=============  Print the home ground names in alphabetical order =================");
+		teams.stream()
+		.map(x->x.getHomeGround().name())
+		.sorted()
+		.forEach(System.out::println);
+		
+	}
+	
 
 }
