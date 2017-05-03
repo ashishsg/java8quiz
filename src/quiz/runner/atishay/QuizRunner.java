5dd05f98b092fa7644caa7349378bb01b6761c51
Atishay Jain <atjain@pdxinc.com>
2017-05-03 13:13:43 +0530

    QuizRunner

--- src/quiz/runner/atishay/QuizRunner.java
+++ src/quiz/runner/atishay/QuizRunner.java
@@ -15,26 +15,46 @@ public class QuizRunner {
 
 		List<Team> teams = QuizDataProvider.getTeams();
 
+		// Case 1
 		System.out.println("***Printing all the team name and home ground***");
-		// Case:1 :Printing Team name and home Ground.
 		teams.stream().forEach(team -> System.out.println(team.getName() + ":" + team.getHomeGround()));
 
+		// Case 2: First Way
 		System.out.println();
-		System.out.println("***Printing the team names in natural sorted order***");
-		// Case:2 :Team names in natural sorted order.
+		System.out.println("***First Way: Printing the team names in natural sorted order***");
 		teams.sort((Team team1, Team team2) -> team1.getName().compareTo(team2.getName()));
 		teams.forEach(team -> System.out.println(team.getName()));
 
+		// Case 2: Second Way
 		System.out.println();
-		System.out.println("***Printing the team names in reverse sorted order***");
-		// Case:2 :Team names in natural sorted order.
+		System.out.println("***Second Way: Printing the home ground names in natural sorted order***");
+		teams.stream().sorted((team1, team2) -> {
+			return team1.getName().compareTo(team2.getName());
+		}).forEach(teamName -> System.out.println(teamName.getName()));
+
+		// Case 3: First Way
+		System.out.println();
+		System.out.println("***First Way:  Printing the team names in reverse sorted order***");
 		teams.sort((Team team1, Team team2) -> team2.getName().compareTo(team1.getName()));
 		teams.forEach(team -> System.out.println(team.getName()));
 
+		// Case 3: Second Way
 		System.out.println();
-		System.out.println("***Printing the home ground names in alphabetical order***");
-		// Case:2 :Team names in natural sorted order.
+		System.out.println("***Second Way: Printing the home ground names in reverse sorted order***");
+		teams.stream().sorted((team1, team2) -> {
+			return team2.getName().compareTo(team1.getName());
+		}).forEach(teamName -> System.out.println(teamName.getName()));
+
+		// Case 4
+		System.out.println();
+		System.out.println("***First Way: Printing the home ground names in alphabetical order***");
 		teams.sort((Team team1, Team team2) -> team1.getHomeGround().name().compareTo(team2.getHomeGround().name()));
 		teams.forEach(team -> System.out.println(team.getHomeGround().name()));
+
+		System.out.println();
+		System.out.println("***Second Way: Printing the home ground names in alphabetical order***");
+		teams.stream().sorted((team1, team2) -> {
+			return team1.getHomeGround().name().compareTo(team2.getHomeGround().name());
+		}).forEach(team -> System.out.println(team.getHomeGround().name()));
 	}
 }
