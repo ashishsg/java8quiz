829ed08e374205c8806f6548fb8a8d178b18ca45
Ashish Gokhale <agokhale@pdxinc.com>
2017-05-04 17:43:56 +0530

    Quiz 3 changes

--- src/quiz/model/Match.java
+++ src/quiz/model/Match.java
@@ -1,5 +1,6 @@
 package quiz.model;
 
+import java.time.LocalDate;
 public class Match {
 	private String opponent;
 	private int runsScored;
@@ -7,8 +8,12 @@ public class Match {
 	private int runsConceded;
 	private int ballsbowled;
 	private Ground venue;
-	
-	public Match(String opponent, int runsScored, int ballsTaken, int runsConceded, int ballsbowled, Ground venue) {
+	private int matchNumber;
+	private LocalDate matchDate;
+	private String team1Name;
+
+	public Match(String opponent, int runsScored, int ballsTaken, int runsConceded, int ballsbowled, Ground venue,
+			int matchNumber, LocalDate matchDate, String team1Name) {
 		super();
 		this.opponent = opponent;
 		this.runsScored = runsScored;
@@ -16,6 +21,9 @@ public class Match {
 		this.runsConceded = runsConceded;
 		this.ballsbowled = ballsbowled;
 		this.venue = venue;
+		this.matchNumber = matchNumber;
+		this.matchDate = matchDate;
+		this.team1Name = team1Name;
 	}
 	
 	public String getOpponent() {
@@ -37,10 +45,52 @@ public class Match {
 		return venue;
 	}
 
+	public int getMatchNumber() {
+		return matchNumber;
+	}
+
+	public LocalDate getMatchDate() {
+		return matchDate;
+	}
+
+
+	public String getTeam1Name() {
+		return team1Name;
+	}
+
 	@Override
 	public String toString() {
 		return "Match [opponent=" + opponent + ", runsScored=" + runsScored + ", ballsTaken=" + ballsTaken
-				+ ", runsConceded=" + runsConceded + ", ballsbowled=" + ballsbowled + ", venue=" + venue + "]";
+				+ ", runsConceded=" + runsConceded + ", ballsbowled=" + ballsbowled + ", venue=" + venue
+				+ ", matchNumber=" + matchNumber + ", matchDate=" + matchDate + ", team1Name=" + team1Name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((matchDate == null) ? 0 : matchDate.hashCode());
+		result = prime * result + matchNumber;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Match other = (Match) obj;
+		if (matchDate == null) {
+			if (other.matchDate != null)
+				return false;
+		} else if (!matchDate.equals(other.matchDate))
+			return false;
+		if (matchNumber != other.matchNumber)
+			return false;
+		return true;
 	}
 	
 	
--- src/quiz/model/QuizDataProvider.java
+++ src/quiz/model/QuizDataProvider.java
@@ -1,5 +1,6 @@
 package quiz.model;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,22 +11,33 @@ public class QuizDataProvider {
 	public static final String TEAM3 = "Mumbai Indians";
 	public static final String TEAM4 = "Kolkata Night Riders";
 	private static final List<Team> teams;
+	private static final LocalDate MATCH1_DATE = LocalDate.of(2017, 4, 1);
+	private static final LocalDate MATCH2_DATE = LocalDate.of(2017, 4, 2);
+	private static final LocalDate MATCH3_DATE = LocalDate.of(2017, 4, 3);
+	private static final LocalDate MATCH4_DATE = LocalDate.of(2017, 4, 4);
+	private static final LocalDate MATCH5_DATE = LocalDate.of(2017, 4, 5);
+	private static final LocalDate MATCH6_DATE = LocalDate.of(2017, 4, 6);
+
 	static {
-		List<Match> team1Matches = Arrays.asList(new Match(TEAM2, 150, 120, 148, 120, Ground.GAHUNJE),
-				new Match(TEAM3, 111, 98, 113, 68, Ground.GAHUNJE),
-				new Match(TEAM4, 190, 120, 168, 120, Ground.EDENGARDENS));
+		List<Match> team1Matches = Arrays.asList(
+				new Match(TEAM2, 150, 120, 148, 120, Ground.GAHUNJE, 1, MATCH1_DATE, TEAM1),
+				new Match(TEAM3, 111, 98, 113, 68, Ground.GAHUNJE, 3, MATCH3_DATE, TEAM1),
+				new Match(TEAM4, 190, 120, 168, 120, Ground.EDENGARDENS, 4, MATCH4_DATE, TEAM1));
 
-		List<Match> team2Matches = Arrays.asList(new Match(TEAM1, 148, 120, 150, 120, Ground.GAHUNJE),
-				new Match(TEAM3, 200, 120, 197, 120, Ground.CHINNASWAMI),
-				new Match(TEAM4, 95, 111, 88, 99, Ground.CHINNASWAMI));
+		List<Match> team2Matches = Arrays.asList(
+				new Match(TEAM1, 148, 120, 150, 120, Ground.GAHUNJE, 1, MATCH1_DATE, TEAM2),
+				new Match(TEAM3, 200, 120, 197, 120, Ground.CHINNASWAMI, 2, MATCH2_DATE, TEAM2),
+				new Match(TEAM4, 95, 111, 88, 99, Ground.CHINNASWAMI, 5, MATCH5_DATE, TEAM2));
 
-		List<Match> team3Matches = Arrays.asList(new Match(TEAM1, 113, 68, 111, 98, Ground.GAHUNJE),
-				new Match(TEAM2, 197, 120, 200, 120, Ground.CHINNASWAMI),
-				new Match(TEAM4, 208, 120, 210, 119, Ground.WANKHEDE));
+		List<Match> team3Matches = Arrays.asList(
+				new Match(TEAM1, 113, 68, 111, 98, Ground.GAHUNJE, 3, MATCH3_DATE, TEAM3),
+				new Match(TEAM2, 197, 120, 200, 120, Ground.CHINNASWAMI, 2, MATCH2_DATE, TEAM3),
+				new Match(TEAM4, 208, 120, 210, 119, Ground.WANKHEDE, 6, MATCH6_DATE, TEAM3));
 
-		List<Match> team4Matches = Arrays.asList(new Match(TEAM1, 168, 120, 190, 120, Ground.EDENGARDENS),
-				new Match(TEAM4, 88, 99, 95, 111, Ground.CHINNASWAMI),
-				new Match(TEAM3, 210, 119, 208, 120, Ground.WANKHEDE));
+		List<Match> team4Matches = Arrays.asList(
+				new Match(TEAM1, 168, 120, 190, 120, Ground.EDENGARDENS, 4, MATCH4_DATE, TEAM4),
+				new Match(TEAM2, 88, 99, 95, 111, Ground.CHINNASWAMI, 5, MATCH5_DATE, TEAM4),
+				new Match(TEAM3, 210, 119, 208, 120, Ground.WANKHEDE, 6, MATCH6_DATE, TEAM4));
 
 		teams = Arrays.asList(new Team(TEAM1, Ground.GAHUNJE, team1Matches),
 				new Team(TEAM2, Ground.CHINNASWAMI, team2Matches), new Team(TEAM3, Ground.WANKHEDE, team3Matches),
