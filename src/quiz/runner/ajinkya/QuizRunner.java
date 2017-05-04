5d60ac0d0517dbf2bd1b4ce587b73f581a044409
Ajinkya Vaze <ajinkyav@gmail.com>
2017-05-04 15:09:15 +0530

    Few corrections

--- src/quiz/runner/ajinkya/QuizRunner.java
+++ src/quiz/runner/ajinkya/QuizRunner.java
@@ -8,72 +8,83 @@ import quiz.model.Team;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+
 public class QuizRunner {
 
-	private QuizRunner() {
+    private QuizRunner() {
 
-	}
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		List<Team> teams = QuizDataProvider.getTeams();
-		//quiz1(teams);
-		quiz2(teams);
-	}
+        List<Team> teams = QuizDataProvider.getTeams();
+        //quiz1(teams);
+        quiz2(teams);
+    }
 
-	private static void quiz1(List<Team> teams) {
-		// 1
-		teams.stream().map(team -> {
-			return new StringBuilder(team.getName()).append(": ").append(team.getHomeGround()).toString();
-		}).forEach(System.out::println);
+    private static void quiz1(List<Team> teams) {
+        // 1
+        teams.stream().map(team -> {
+            return new StringBuilder(team.getName()).append(": ").append(team.getHomeGround()).toString();
+        }).forEach(System.out::println);
 
-		// 2
-		teams.stream().map(team -> {
-			return team.getName();
-		}).sorted().forEach(System.out::println);
-		
-		// 3
-		teams.stream().map(team -> {
-			return team.getName();
-		}).sorted(Collections.reverseOrder()).forEach(System.out::println);
-		
-		// 4
-		teams.stream().map(team -> {
-			return team.getHomeGround().name();
-		}).sorted().forEach(System.out::println);
-	}
-	
-	private static void quiz2(List<Team> teams) {
-		// 1
-		teams.stream().collect(Collectors.toMap(Team::getName, Team::getHomeGround)).entrySet().forEach(System.out::println);
+        // 2
+        teams.stream().map(team -> {
+            return team.getName();
+        }).sorted().forEach(System.out::println);
 
-		// 2
-		teams.stream()
-				.filter(team -> QuizDataProvider.TEAM1.equals(team.getName()))
-				.flatMap(team -> team.getMatches().stream())
-				.collect(Collectors.groupingBy(Match::getVenue, Collectors.counting()))
-				.entrySet().stream()
-				.forEach(System.out::println);
+        // 3
+        teams.stream().map(team -> {
+            return team.getName();
+        }).sorted(Collections.reverseOrder()).forEach(System.out::println);
 
-		// 3
-		System.out.println(teams.stream()
-				.flatMap(team -> team.getMatches().stream())
-				.filter(match -> QuizDataProvider.TEAM1.equals(match.getOpponent()))
-				.mapToInt(Match::getRunsScored)
-				.sum());
+        // 4
+        teams.stream().map(team -> {
+            return team.getHomeGround().name();
+        }).sorted().forEach(System.out::println);
+    }
 
-		// 4
-		boolean playedAtVenue = teams.stream()
-				.filter(team -> QuizDataProvider.TEAM1.equals(team.getName()))
-				.flatMap(team -> team.getMatches().stream())
-				.anyMatch(match -> Ground.WANKHEDE == match.getVenue());
-		System.out.println(playedAtVenue ? "YES": "NO");
+    private static void quiz2(List<Team> teams) {
+        // 1
+        teams.stream().collect(Collectors.toMap(Team::getName, Team::getHomeGround)).entrySet().forEach(System.out::println);
 
-		// 5
-		System.out.println(teams.stream()
-				.filter(team -> QuizDataProvider.TEAM1.equals(team.getName()))
-				.flatMap(team -> team.getMatches().stream())
-				.filter(match -> match.getRunsScored() > match.getRunsConceded())
-				.count());
-	}
+        // 2
+        teams.stream()
+                .filter(team -> QuizDataProvider.TEAM1.equals(team.getName()))
+                .findAny().ifPresent(
+                (team) -> team.getMatches().stream()
+                        .collect(Collectors.groupingBy(Match::getVenue, Collectors.counting()))
+                        .entrySet().stream()
+                        .forEach(System.out::println));
+
+        // 3
+        teams.stream()
+                .filter(team -> QuizDataProvider.TEAM1.equals(team.getName()))
+                .findAny().ifPresent(
+                (team) -> {
+                    int runsScored = team.getMatches().stream()
+                            .mapToInt(Match::getRunsScored)
+                            .sum();
+                    System.out.println(runsScored);
+                });
+
+        // 4
+        teams.stream()
+                .filter(team -> QuizDataProvider.TEAM1.equals(team.getName()))
+                .findAny().ifPresent(team -> {
+            boolean playedAtVenue = team.getMatches().stream()
+                    .anyMatch(match -> Ground.WANKHEDE == match.getVenue());
+            System.out.println(playedAtVenue ? "YES" : "NO");
+        });
+
+        // 5
+        teams.stream()
+                .filter(team -> QuizDataProvider.TEAM1.equals(team.getName()))
+                .findAny().ifPresent(team -> {
+            long count = team.getMatches().stream()
+                    .filter(match -> match.getRunsScored() > match.getRunsConceded())
+                    .count();
+            System.out.println(count);
+        });
+    }
 }
