8af36d84c8109bc5fc17e13d3231d42725b9ed6e
Mayank Saxena <mayanks@synerzipune.local>
2017-05-10 16:27:24 +0530

    Quiz 4 Answer

--- src/quiz/runner/mayank/QuizRunner.java
+++ src/quiz/runner/mayank/QuizRunner.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import quiz.model.Match;
 import quiz.model.QuizDataProvider;
 import quiz.model.Team;
 
@@ -24,7 +25,8 @@ public class QuizRunner {
 	}
 
 	public static void main(String[] args) {
-		quiz3();
+//		quiz3();
+		quiz4();
 	}
 
 	private static void quiz3() {
@@ -32,7 +34,7 @@ public class QuizRunner {
 
 		System.out.println("******** Matches played on 4th April 2017 *******");
 		teams.stream().forEach(t -> t.getMatches().stream()
-				.filter(p -> LocalDate.of(2017, 4, 4).equals(p.getMatchDate())).forEach(System.out::println));
+				.filter(p -> LocalDate.of(2017, 4, 4).equals(p.getMatchDate())).distinct().forEach(System.out::println));
 
 		System.out.println("******** Duration of the Tournament *******");
 		Set<LocalDate> set = teams.stream().flatMap(team -> team.getMatches().stream()).collect(Collectors.toList())
@@ -55,5 +57,38 @@ public class QuizRunner {
 				RoundingMode.HALF_UP)).subtract(ballsFaced);
 		System.out.println("Maximum Balls Required : " + maxBalls.intValue());
 	}
+	
+	private static void quiz4() {
+		List<Team> teams = QuizDataProvider.getTeams();
+		
+		System.out.println("******** Dates when matches were played at each venue *******");
+		teams.stream().flatMap(team -> team.getMatches().stream())
+		.distinct()
+		.forEach(match -> {
+			System.out.println(match.getVenue() + " =[" + match.getMatchDate() + "]");
+		});
 
+		System.out.println("******** Match in which maximum number of runs were scored at each venue *******");
+		teams.stream().flatMap(team -> team.getMatches().stream())
+		.collect(Collectors.groupingBy(Match::getVenue, Collectors.maxBy((m1, m2) -> {
+					return Integer.valueOf(m1.getRunsScored()).compareTo(Integer.valueOf(m2.getRunsScored()));
+				})))
+		.entrySet().stream()
+		.forEach(m -> {
+					System.out.println(m.getKey() + " : " + m.getValue().get());
+				});
+
+		System.out.println("******** Total runs scored at each venue *******");
+		teams.stream().flatMap(team -> team.getMatches().stream())
+		.collect(Collectors.groupingBy(Match::getVenue, Collectors.summingInt(Match::getRunsScored)))
+		.entrySet().stream()
+		.forEach(System.out::println);
+		
+		System.out.println("******** Mapping match numbers to list as day match or night match *******");
+		teams.stream().flatMap(team -> team.getMatches().stream())
+		.distinct()
+		.collect(Collectors.groupingBy(Match::isDay,Collectors.mapping(Match::getMatchNumber,Collectors.toList())))
+		.entrySet().stream()
+		.forEach(System.out::println);
+	}
 }
